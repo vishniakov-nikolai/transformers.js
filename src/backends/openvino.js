d51209c8df9e0bc8f6c82c6a9754422984eec7bd
Vishniakov <nikolai.vishniakov@intel.com>
2024-09-09 14:49:34 +0200

    Fix sort filenames

--- src/backends/openvino.js
+++ src/backends/openvino.js
@@ -13,9 +13,10 @@ module.exports = { create: getWrappedOVModelByPath };
 async function getWrappedOVModelByPath(modelDir, filename, options) {
   const device = options.device || 'AUTO';
   const filenames = Array.isArray(filename) ? filename : [filename];
-  const modelFiles = [];
+  const sortedFilenames = sortFilenames(filenames);
 
-  for (const filename of filenames) {
+  const modelFiles = [];
+  for (const filename of sortedFilenames) {
       const file = await getModelFile(modelDir, filename, true, options);
 
       modelFiles.push(file);
@@ -26,7 +27,10 @@ async function getWrappedOVModelByPath(modelDir, filename, options) {
     GLOBAL_CORE = new ov.Core();
   }
 
-  const model = await getModel(modelFiles);
+  const model = modelFiles.length === 2
+    ? await GLOBAL_CORE.readModel(modelFiles[0], modelFiles[1])
+    : await GLOBAL_CORE.readModel(modelFiles[0]);
+  console.log('== here')
   const inputNames = model.inputs.map(i => i.toString());
   const compiledModel = await GLOBAL_CORE.compileModel(model, device);
   const ir = compiledModel.createInferRequest();
@@ -94,22 +98,19 @@ async function getWrappedOVModelByPath(modelDir, filename, options) {
   }
 }
 
-function getModel(files) {
-    const count = files.length;
+function sortFilenames(files) {
     const isXMLorBIN = new RegExp(/^.+\.(xml|bin)$/i);
-    const isXML = new RegExp(/^.+\.(xml)$/i);
-    const isBIN = new RegExp(/^.+\.(bin)$/i);
-    const isIRModel = count === 2
+    const isIRModel = files.length === 2
         && files.filter(name => isXMLorBIN.test(name)).length === 2;
 
-    if (!isIRModel)
-        return count === 2
-            ? GLOBAL_CORE.readModel(files[0], files[1])
-            : GLOBAL_CORE.readModel(files[0]);
+    if (!isIRModel) return files;
+
+    const isXML = new RegExp(/^.+\.(xml)$/i);
+    const isBIN = new RegExp(/^.+\.(bin)$/i);
 
     // Order is important
     const xml = files.find(isXML.test);
     const bin = files.find(isBIN.test);
 
-    return GLOBAL_CORE.readModel(xml, bin);
+    return [xml, bin];
 }
